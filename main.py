b4d9a4f516a7b8486ea2f5a958caf0945b0c7cdc
Alejandro <79983176+yamac614mobile@users.noreply.github.com>
2021-04-07 01:26:39 +0200

    d

--- main.py
+++ main.py
@@ -127,5 +127,5 @@ with d2:
     tabla = pd.read_csv("Tabla Resultados.csv")
     totaldist = pd.DataFrame(tabla['Distancia'])
     st.write(totaldist)
-    st.write(sum(tabla))
+    st.write(sum(tabla[]))
 # %%
